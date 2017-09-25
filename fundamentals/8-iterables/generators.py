a8dbb7c2d0ae90b374c3f5bcf218699a686adb8b
the-james-burton <the.james.burton@gmail.com>
2017-09-25 19:45:44 +0100

    stateful generator

--- fundamentals/8-iterables/generators.py
+++ fundamentals/8-iterables/generators.py
@@ -11,12 +11,25 @@ def gen123():
     yield 3
 
 
+def distinct(iterable):
+    seen = set()
+    for item in iterable:
+        if item in seen:
+            continue
+        seen.add(item)
+        yield item
+
+
+
 def main():
     # must get an instance first, since they are stateful...
     g = gen123()
     print(next(g))
     print(next(g))
     print(next(g))
+    items = [2, 3, 3, 4, 4, 4, 5, 7, 7, 9]
+    for item in distinct(items):
+        print(item)
 
 
 if __name__ == '__main__':

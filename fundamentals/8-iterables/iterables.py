cf56a663a1ce48716c7e6ad92be1d5ed3f0822f8
the-james-burton <the.james.burton@gmail.com>
2017-09-26 07:14:53 +0100

    extract is_prime to module

--- fundamentals/8-iterables/iterables.py
+++ fundamentals/8-iterables/iterables.py
@@ -5,17 +5,22 @@ import sys
 import math
 import os
 import glob
+import prime
 from pprint import pprint as pp
+
 
 country_to_city = {'France': 'Paris',
                    'Russia': 'Moscow',
                    'Japan': 'Tokyo',
                    'India': 'Mubmai'}
+
+
 weekdays = ['Monday',
             'Tuesday',
             'Wednesday',
             'Thursday',
             'Friday',]
+
 
 def iterate_weekdays():
     iterable = iter(weekdays)
@@ -42,21 +47,12 @@ def files_sizes(path):
     return {os.path.realpath(p): os.stat(p).st_size for p in glob.glob(path)}
 
 
-def is_prime(n):
-    if n == 2 or n == 3: return True
-    if n % 2 == 0 or n < 2: return False
-    for i in range(3, int(n ** 0.5) + 1, 2):
-        if n % i == 0:
-            return False
-    return True
-
-
 def primes():
-    return [x for x in range(101) if is_prime(x)]
+    return [x for x in range(101) if prime.is_prime(x)]
 
 
 def prime_squares():
-    return {x * x:(1, x, x * x) for x in range(101) if is_prime(x)}
+    return {x * x:(1, x, x * x) for x in range(101) if prime.is_prime(x)}
 
 
 def main():
@@ -68,6 +64,7 @@ def main():
     pp(primes())
     pp(prime_squares())
     iterate_weekdays()
+
 
 if __name__ == '__main__':
     main()

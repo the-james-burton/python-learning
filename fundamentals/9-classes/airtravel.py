0905bcb20e7f1116fa2eb8bd68ff9c5e032f0d15
the-james-burton <the.james.burton@gmail.com>
2017-09-27 18:47:13 +0100

    added init, validation and getter

--- fundamentals/9-classes/airtravel.py
+++ fundamentals/9-classes/airtravel.py
@@ -2,5 +2,15 @@
 
 
 class Flight:
+    # this is an init not a constructor, since the instance already exists when it is called...
+    def __init__(self, number):
+        # validations...
+        if not number[:2].isalpha():
+            raise ValueError("No airline code in:{}".format(number))
+        self._number = number
+
     def number(self):
-        print("AB123")
+        return self._number
+
+    def airline(self):
+        return self._number[:2]

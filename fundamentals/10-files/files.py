1dc00b7ec6a4cf1d65667ec9382544b81405b4c9
the-james-burton <the.james.burton@gmail.com>
2017-10-04 07:28:13 +0100

    lorem ipsum

---
+++ fundamentals/10-files/files.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""using files"""
+
+ipsum = (
+    "Rerum pariatur aliquam corrupti possimus ipsum itaque. Voluptatem officia doloremque quos esse quo voluptatem quo. Rem est alias velit perspiciatis. Ipsum quo ea aliquam.\n"
+    "Quia ut rerum minima tempore. Sunt in nisi totam magnam consequatur. Impedit alias cum provident ut illum esse.\n"
+    "Quam sunt nemo sed ut laborum velit. Corrupti rerum reiciendis temporibus numquam ducimus eveniet. Voluptatum et in recusandae.\n"
+    "Iusto ipsam nihil enim neque rerum modi. Consectetur officia ut voluptas laudantium. Enim fugiat voluptas quisquam odit veniam similique.\n"
+    "Illum quidem fuga voluptatem atque. Incidunt magnam alias ad at ut et est et. Repudiandae voluptatem quisquam est. A voluptatem vel modi. Officia aut laborum non voluptates dolor ex error.\n"
+)
+
+
+def main():
+    print(ipsum)
+
+
+if __name__ == '__main__':
+    main()

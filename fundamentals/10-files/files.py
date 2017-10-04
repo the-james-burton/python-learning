255fbf76e8d52db2ef86becf9cc5f6c4ef380521
the-james-burton <the.james.burton@gmail.com>
2017-10-04 19:35:41 +0100

    write and read file

--- fundamentals/10-files/files.py
+++ fundamentals/10-files/files.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 """using files"""
 
-ipsum = (
-    "Rerum pariatur aliquam corrupti possimus ipsum itaque. Voluptatem officia doloremque quos esse quo voluptatem quo. Rem est alias velit perspiciatis. Ipsum quo ea aliquam.\n"
-    "Quia ut rerum minima tempore. Sunt in nisi totam magnam consequatur. Impedit alias cum provident ut illum esse.\n"
-    "Quam sunt nemo sed ut laborum velit. Corrupti rerum reiciendis temporibus numquam ducimus eveniet. Voluptatum et in recusandae.\n"
-    "Iusto ipsam nihil enim neque rerum modi. Consectetur officia ut voluptas laudantium. Enim fugiat voluptas quisquam odit veniam similique.\n"
-    "Illum quidem fuga voluptatem atque. Incidunt magnam alias ad at ut et est et. Repudiandae voluptatem quisquam est. A voluptatem vel modi. Officia aut laborum non voluptates dolor ex error.\n"
-)
+import sys
+from pprint import pprint
+
+
+ipsum1 = "Rerum pariatur aliquam corrupti possimus ipsum itaque. Voluptatem officia doloremque quos esse quo voluptatem quo. Rem est alias velit perspiciatis. Ipsum quo ea aliquam.\n"
+ipsum2 = "Quia ut rerum minima tempore. Sunt in nisi totam magnam consequatur. Impedit alias cum provident ut illum esse.\n"
+ipsum3 = "Quam sunt nemo sed ut laborum velit. Corrupti rerum reiciendis temporibus numquam ducimus eveniet. Voluptatum et in recusandae.\n"
+ipsum4 = "Iusto ipsam nihil enim neque rerum modi. Consectetur officia ut voluptas laudantium. Enim fugiat voluptas quisquam odit veniam similique.\n"
+ipsum5 = "Illum quidem fuga voluptatem atque. Incidunt magnam alias ad at ut et est et. Repudiandae voluptatem quisquam est. A voluptatem vel modi. Officia aut laborum non voluptates dolor ex error.\n"
 
 
 def main():
-    print(ipsum)
+    f = open("temp.txt", mode="wt", encoding="utf-8")
+    f.write(ipsum1)
+    f.write(ipsum2)
+    f.write(ipsum3)
+    f.write(ipsum4)
+    f.write(ipsum5)
+    f.close()
 
+    g = open("temp.txt", mode="rt", encoding="utf-8")
+    print(g.read(len(ipsum1)))
+    print(g.readline())
+    print(g.readlines())
+    g.close()
 
 if __name__ == '__main__':
     main()

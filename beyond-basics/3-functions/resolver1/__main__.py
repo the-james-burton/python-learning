8dfecd626dbcc6dc2a85524ade8b5e1fa7b9805b
the-james-burton <the.james.burton@gmail.com>
2017-10-18 07:35:18 +0100

    simple use of timeit in main

--- beyond-basics/3-functions/resolver1/__main__.py
+++ beyond-basics/3-functions/resolver1/__main__.py
@@ -1,8 +1,17 @@
 print('hello...')
 
 from resolver1 import resolver
+import timeit
+
+def resolve_timed(func, param):
+    start_time = timeit.default_timer()
+    func(param)
+    time = timeit.default_timer() - start_time
+    print("{:f}".format(time))
+
 
 resolve = resolver.Resolver()
+resolve_timed(resolve, 'python.org')
+resolve_timed(resolve, 'python.org')
 
-print(resolve('python.org'))
 print(resolve._cache)

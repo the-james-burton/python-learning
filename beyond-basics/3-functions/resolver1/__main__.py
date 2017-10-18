0c8d6dd566c15f11a8d87dc52a920f4f2fdb7ec1
the-james-burton <the.james.burton@gmail.com>
2017-10-18 07:14:53 +0100

    ip resolver and main

---
+++ beyond-basics/3-functions/resolver1/__main__.py
@@ -0,0 +1,7 @@
+print('hello...')
+
+from resolver1 import resolver
+
+resolve = resolver.Resolver()
+
+print(resolve('python.org'))
---
+++ beyond-basics/3-functions/resolver1/resolver.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Demostrate functions"""
+
+
+import socket
+
+
+class Resolver:
+
+    def __init__(self):
+        self._cache = {}
+
+    def __call__(self, host):
+        if host not in self._cache:
+            self._cache[host] = socket.gethostbyname(host)
+        return self._cache[host]
+
+

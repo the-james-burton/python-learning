a5a5a97269a1f34c279ac1dd54de25aad06db1f8
the-james-burton <the.james.burton@gmail.com>
2017-10-23 07:34:37 +0100

    extended arguments *

---
+++ beyond-basics/3-functions/arguments/__main__.py
@@ -0,0 +1,23 @@
+print('functions...')
+
+
+import arguments
+
+print(arguments.hypervolume(2))
+print(arguments.hypervolume(2, 2))
+print(arguments.hypervolume(2, 2, 2))
+try:
+    print(arguments.hypervolume())
+except BaseException as e:
+    print(type(e))
+
+print(arguments.hypervolume2(2))
+print(arguments.hypervolume2(2, 2))
+print(arguments.hypervolume2(2, 2, 2))
+
+try:
+    print(arguments.hypervolume2())
+except TypeError as e:
+    print(e)
+
+
---
+++ beyond-basics/3-functions/arguments/arguments.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""functions to demonstrate extended arguments"""
+
+def hypervolume(*lengths):
+    """will raise a unwanted StopIteration if no lengths"""
+    i = iter(lengths)
+    v = next(i)
+    for length in i:
+        v *= length
+    return v
+
+def hypervolume2(first_length, *next_lengths):
+    """although there are two parameters, this does not
+    change the interface for callers"""
+    v = first_length
+    for l in next_lengths:
+        v *= l
+    return v

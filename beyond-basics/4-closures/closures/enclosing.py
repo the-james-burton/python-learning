9b8dac820eb96e75f05b111fae6f3c820efd6af9
the-james-burton <the.james.burton@gmail.com>
2017-10-26 07:12:00 +0100

    nonlocal keyword

--- beyond-basics/4-closures/closures/enclosing.py
+++ beyond-basics/4-closures/closures/enclosing.py
@@ -5,7 +5,11 @@ def enclosing():
     message = 'enclosing'
 
     def local():
-        global message
+        # use 'global' to bring a global variable into this scope...
+        # global message
+        # use 'nonlocal' to bring the variable in the next
+        # wider scope into this scope...
+        nonlocal message
         message = 'local'
 
     print('enclosing message:', message)

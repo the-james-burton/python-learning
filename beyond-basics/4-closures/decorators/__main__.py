b67593f6bfc1eab0f488aebb21b23c50aa575d8e
the-james-burton <the.james.burton@gmail.com>
2017-10-26 07:30:34 +0100

    class decorator

--- beyond-basics/4-closures/decorators/__main__.py
+++ beyond-basics/4-closures/decorators/__main__.py
@@ -1,6 +1,12 @@
 print('decorators...')
 
-from decorators.decorate import get_city, get_city2
+from decorators.decorate import get_city, get_city2, hello
 
 print(get_city())
 print(get_city2())
+
+hello('Mary')
+hello('Norma')
+hello('Oscar')
+hello('Patty')
+print(hello.count)
--- beyond-basics/4-closures/decorators/decorate.py
+++ beyond-basics/4-closures/decorators/decorate.py
@@ -15,3 +15,23 @@ def get_city():
 @escape_unicode
 def get_city2():
     return city
+
+
+class CallCount:
+    def __init__(self, f):
+        self.f = f
+        # this property will be visible when a function is decorated
+        # with this class...
+        self.count = 0
+
+    def __call__(self, *args, **kwargs):
+        """ classes MUST implement this method
+        to be used as a decorator"""
+        self.count += 1
+        return self.f(*args, **kwargs)
+
+
+@CallCount
+def hello(name):
+    print('Hello, {}'.format(name))
+

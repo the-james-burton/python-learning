4a6bbdc0e220c13a028f18efb9bda08487dcaaa0
the-james-burton <the.james.burton@gmail.com>
2017-10-30 06:55:15 +0000

    shipping container class

---
+++ beyond-basics/5-methods/__main__.py
@@ -0,0 +1,6 @@
+print('methods...')
+
+from shipping import *
+
+c1 = ShippingContainer('ABC', 'Toys')
+print(c1.owner)
---
+++ beyond-basics/5-methods/shipping.py
@@ -0,0 +1,6 @@
+
+class ShippingContainer:
+
+    def __init__(self, owner, contents):
+        self.owner = owner
+        self.contents = contents

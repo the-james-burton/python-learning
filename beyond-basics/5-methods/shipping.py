e43449979fbd52ca71924779bd5329cf1d08e149
the-james-burton <the.james.burton@gmail.com>
2017-10-30 07:13:51 +0000

    static method

--- beyond-basics/5-methods/shipping.py
+++ beyond-basics/5-methods/shipping.py
@@ -3,9 +3,20 @@ class ShippingContainer:
 
     next_serial = 1234
 
+
+    @staticmethod
+    def _get_next_serial(self):
+        result = ShippingContainer.next_serial
+        ShippingContainer.next_serial += 1
+        return result
+
+
     def __init__(self, owner, contents):
         self.owner = owner
         self.contents = contents
+        # can call it directly...
         # next_serial is not in the LEGB scopes, so must be qualified...
         self.serial = ShippingContainer.next_serial
         ShippingContainer.next_serial += 1
+        #... or use the static method...
+        self.serial = ShippingContainer._get_next_serial(self)

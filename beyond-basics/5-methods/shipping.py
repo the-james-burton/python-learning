dda0a2d1d1e04c6928fa7cf1ea4b6170f771fb53
the-james-burton <the.james.burton@gmail.com>
2017-10-30 07:38:52 +0000

    static function

--- beyond-basics/5-methods/shipping.py
+++ beyond-basics/5-methods/shipping.py
@@ -1,18 +1,22 @@
 
+import random
+
 class ShippingContainer:
 
     next_serial = 1234
 
 
     @staticmethod
-    def _get_next_serial():
+    def _get_next_serial_random():
         # note there is no 'self' argument
-        result = ShippingContainer.next_serial
-        ShippingContainer.next_serial += 1
-        return result
+        # static methods should be used when no access to the class is required
+        # i.e. a 'pure' function that depends only on its arguments...
+        return random.randint(0, 9999)
 
     @classmethod
-    def _get_next_serial2(cls):
+    def _get_next_serial_incremented(cls):
+        # class methods should be used when access to the class is required
+        # in this case, to maintain state across instances...
         result = cls.next_serial
         cls.next_serial += 1
         return result
@@ -38,6 +42,6 @@ class ShippingContainer:
         self.serial = ShippingContainer.next_serial
         ShippingContainer.next_serial += 1
         #... or use the static method...
-        self.serial = ShippingContainer._get_next_serial()
+        self.serial = ShippingContainer._get_next_serial_random()
         #... or use the class method...
-        self.serial = ShippingContainer._get_next_serial2()
+        # self.serial = ShippingContainer._get_next_incremented()

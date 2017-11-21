da16fa2514e5afc80373084bba4529508e0daf09
the-james-burton <the.james.burton@gmail.com>
2017-11-21 18:56:58 +0000

    unittest skeleton

--- beyond-basics/9-inheritance/sorted_list.py
+++ beyond-basics/9-inheritance/sorted_list.py
@@ -16,3 +16,16 @@ class SimpleList:
 
     def __repr__(self):
         return "SimpleList({!r})".format(self._items)
+
+
+class SortedList(SimpleList):
+    def __init__(self, items=()):
+        super().__init__(items)
+        self.sort()
+
+    def add(self, item):
+        super().add(item)
+        self.sort()
+
+    def __repr__(self):
+        return "SortedList({!r})".format(list(self))
---
+++ beyond-basics/9-inheritance/sorted_list_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""sorted_list test"""
+
+import unittest
+import sorted_list
+
+
+class SortedListTest(unittest.TestCase):
+
+    def test_sorted_list(self):
+        print("tested!")
+
+if __name__  == '__main__':
+    """run all tests in this module"""
+    unittest.main()

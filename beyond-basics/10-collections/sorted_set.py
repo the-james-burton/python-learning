b9025e69a39e698ccfbcbc5bf8e12ff3e83a56c1
the-james-burton <the.james.burton@gmail.com>
2017-11-22 07:29:21 +0000

    implement container protocol with tests

--- beyond-basics/10-collections/sorted_set.py
+++ beyond-basics/10-collections/sorted_set.py
@@ -3,4 +3,5 @@ class SortedSet:
     def __init__(self, items=None):
         self._items = sorted(set(items)) if items is not None else []
 
-
+    def __contains__(self, item):
+        return item in self._items

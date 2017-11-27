e3d7bcf57a33c9fda329cd04143e0a086d7b3268
the-james-burton <the.james.burton@gmail.com>
2017-11-27 06:56:25 +0000

    implement sized protocol with tests

--- beyond-basics/10-collections/sorted_set.py
+++ beyond-basics/10-collections/sorted_set.py
@@ -1,7 +1,14 @@
 class SortedSet:
 
     def __init__(self, items=None):
+        """if no starting list is given, then initialize
+            with a new empty list"""
         self._items = sorted(set(items)) if items is not None else []
 
     def __contains__(self, item):
+        """implements the container protocol"""
         return item in self._items
+
+    def __len__(self):
+        """implements the sized protocol"""
+        return len(self._items)

cc1ea5c8b844127428ae1c541c8ce42485b28c9f
the-james-burton <the.james.burton@gmail.com>
2018-01-23 07:17:57 +0000

    exception inheritance

--- beyond-basics/11-exceptions/handler.py
+++ beyond-basics/11-exceptions/handler.py
@@ -4,15 +4,32 @@ from random import randrange
 
 
 def main():
-    number = randrange(100)
+    number = randrange(10)
     while True:
         try:
             guess = int(input("? "))
+        # ALWAYS specify an exception type...
         except ValueError:
             continue
         if guess == number:
             print("You win!")
             break
+
+def demo_lookup_error():
+    s = [1, 4, 6]
+    try:
+        item = s[5]
+    # IndexError inherits from LookupError
+    except LookupError:
+        print("Handled IndexError")
+
+    d = dict(a=65, b=66, c=67)
+    try:
+        value = d['x']
+    # KeyError also inherits from LookupError
+    except LookupError:
+        print("Handled KeyError")
+
 
 if __name__ == '__main__':
     main()
--- beyond-basics/11-exceptions/handler_test.py
+++ beyond-basics/11-exceptions/handler_test.py
@@ -10,3 +10,6 @@ class TestHandler(unittest.TestCase):
         sys.stdin = io.StringIO('asdlkj')
         print(input())
         # handler.main()
+
+    def test_demo_lookup_error(self):
+        handler.demo_lookup_error()

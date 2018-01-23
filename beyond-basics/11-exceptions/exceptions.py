5a37a07de0d559ad630b1355fb3c7bf0081fab47
the-james-burton <the.james.burton@gmail.com>
2018-01-23 07:26:03 +0000

    added median function

--- beyond-basics/11-exceptions/exceptions.py
+++ beyond-basics/11-exceptions/exceptions.py
@@ -30,6 +30,24 @@ def demo_lookup_error():
     except LookupError:
         print("Handled KeyError")
 
+def median(iterable):
+    """Obtain the central value of a series.
+
+    Sorts the iterable and returns the middle value if there is an even
+    number of elements, or the arithmetic mean of the middle two elements
+    if there is an even number of elements.
+
+    Args:
+        iterable: A series of orderable items.
+
+    Returns:
+        The median value.
+    """
+    items = sorted(iterable)
+    median_index = (len(items) - 1) // 2
+    if len(items) % 2 != 0:
+        return items[median_index]
+    return (items[median_index] + items[median_index + 1]) / 2.0
 
 if __name__ == '__main__':
     main()
--- beyond-basics/11-exceptions/exceptions_test.py
+++ beyond-basics/11-exceptions/exceptions_test.py
@@ -13,3 +13,6 @@ class TestHandler(unittest.TestCase):
 
     def test_demo_lookup_error(self):
         exceptions.demo_lookup_error()
+
+    def test_median(self):
+        print(exceptions.median([5,2,6,4,7,9]))

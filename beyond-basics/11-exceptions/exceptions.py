a0f354da7ea0f631138ee7d2e7b0bddd2c3fd393
the-james-burton <the.james.burton@gmail.com>
2018-01-23 19:29:54 +0000

    custom TriangleError exception

--- beyond-basics/11-exceptions/exceptions.py
+++ beyond-basics/11-exceptions/exceptions.py
@@ -56,7 +56,14 @@ def median(iterable):
 def demo_unicode_error():
     b'hello\x81world'.decode('utf-8')
 
+
+class TriangleError(Exception):
+    pass
+
 def triangle_area(a, b, c):
+    sides = sorted((a, b, c))
+    if sides[2] > sides[0] + sides[1]:
+        raise TriangleError("Illegal triangle",)
     p = (a + b + c) / 2
     a = math.sqrt(p * (p - a) * (p - b) * (p - c))
     return a

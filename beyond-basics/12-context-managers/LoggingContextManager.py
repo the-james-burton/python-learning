eebe55b6e9f2c70fa86ea9b10af8190bc90b6fdf
the-james-burton <the.james.burton@gmail.com>
2018-01-31 07:13:06 +0000

    added logging

--- beyond-basics/12-context-managers/LoggingContextManager.py
+++ beyond-basics/12-context-managers/LoggingContextManager.py
@@ -1,7 +1,10 @@
 class LoggingContextManager:
 
     def __enter__(self):
+        print('LoggingContextManager.__enter__()')
         return "you are in a with-block"
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        print('LoggingContextManager.__exit__() [type:{}, value:{}, traceback:{}]'
+            .format(exc_type, exc_val, exc_tb))
         return

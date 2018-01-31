b42deac902265b168ab6799c4a7bb55ada119391
the-james-burton <the.james.burton@gmail.com>
2018-01-31 07:09:33 +0000

    __enter__() return value is passed out

--- beyond-basics/12-context-managers/LoggingContextManager.py
+++ beyond-basics/12-context-managers/LoggingContextManager.py
@@ -1,7 +1,7 @@
 class LoggingContextManager:
 
     def __enter__(self):
-        return self
+        return "you are in a with-block"
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         return

fb811845b89322f1faa99b93ae6abb131cbf66eb
the-james-burton <the.james.burton@gmail.com>
2018-01-31 07:16:31 +0000

    added logging

--- beyond-basics/12-context-managers/LoggingContextManager_test.py
+++ beyond-basics/12-context-managers/LoggingContextManager_test.py
@@ -3,6 +3,10 @@ from LoggingContextManager import *
 
 class TestHandler(unittest.TestCase):
 
-    def testLoggingContextManager(self):
+    def testLoggingContextManagerNoException(self):
         with LoggingContextManager() as cm:
             print(cm)
+
+    def testLoggingContextManagerWithException(self):
+        with LoggingContextManager() as cm:
+            raise ValueError('I hate you')

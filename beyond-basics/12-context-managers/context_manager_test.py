d1fa627025a8db3a89f0049623def30c91575482
the-james-burton <the.james.burton@gmail.com>
2018-02-01 07:16:43 +0000

    line splits

--- beyond-basics/12-context-managers/context_manager_test.py
+++ beyond-basics/12-context-managers/context_manager_test.py
@@ -13,15 +13,16 @@ class TestHandler(unittest.TestCase):
             raise ValueError('I hate you')
 
     def testAnnotatedCLoggingContextManager(self):
-        with annotated_logging_context_manager() as cm:
+        with annotated_logging_context_manager('cm') as cm:
             print(cm)
 
     def testAnnotatedCLoggingContextManagerWithException(self):
-        with annotated_logging_context_manager() as cm:
+        with annotated_logging_context_manager('cm') as cm:
             raise ValueError('I still hate you')
 
     def testNestedContextManagers(self):
-        with annotated_logging_context_manager('inner1') as cm1, annotated_logging_context_manager('outer1') as cm2:
+        with annotated_logging_context_manager('inner1') as cm1,\
+             annotated_logging_context_manager('outer1') as cm2:
             print('cm1:{}, cm2:{}'.format(cm1, cm2))
         # eqivalent to...
         with annotated_logging_context_manager('inner2') as cm1:

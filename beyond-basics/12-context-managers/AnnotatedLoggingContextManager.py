bd81ba6167791098b2b641686a9fbacca46e59fc
the-james-burton <the.james.burton@gmail.com>
2018-02-01 07:13:11 +0000

    nesting context managers

--- beyond-basics/12-context-managers/AnnotatedLoggingContextManager.py
+++ beyond-basics/12-context-managers/AnnotatedLoggingContextManager.py
@@ -3,14 +3,14 @@ import sys
 
 
 @contextlib.contextmanager
-def annotated_logging_context_manager():
-    print('LoggingContextManager.__enter__()')
+def annotated_logging_context_manager(name):
+    print('LoggingContextManager.__enter__({})'.format(name))
     try:
-        yield "You're in a with-block!"
-        print('LoggingContextManager.__exit__()')
+        yield "{}: You're in a with-block!".format(name)
+        print('LoggingContextManager.__exit__({})'.format(name))
     except Exception:
-        print('LoggingContextManager.__exit__() [exception:{}]'
-              .format(sys.exc_info()))
+        print('LoggingContextManager.__exit__({}) [exception:{}]'
+              .format(name, sys.exc_info()))
         # if you don't want to propagate the exception
         # don't call raise here...
         raise
--- beyond-basics/12-context-managers/context_manager_test.py
+++ beyond-basics/12-context-managers/context_manager_test.py
@@ -19,3 +19,11 @@ class TestHandler(unittest.TestCase):
     def testAnnotatedCLoggingContextManagerWithException(self):
         with annotated_logging_context_manager() as cm:
             raise ValueError('I still hate you')
+
+    def testNestedContextManagers(self):
+        with annotated_logging_context_manager('inner1') as cm1, annotated_logging_context_manager('outer1') as cm2:
+            print('cm1:{}, cm2:{}'.format(cm1, cm2))
+        # eqivalent to...
+        with annotated_logging_context_manager('inner2') as cm1:
+            with annotated_logging_context_manager('outer2') as cm2:
+                print('cm1:{}, cm2:{}'.format(cm1, cm2))

d965c9e4e83beb8252ca4c764cf9c460b92f39cc
the-james-burton <the.james.burton@gmail.com>
2018-02-01 06:47:49 +0000

    different logging for exception

--- beyond-basics/12-context-managers/LoggingContextManager.py
+++ beyond-basics/12-context-managers/LoggingContextManager.py
@@ -5,6 +5,12 @@ class LoggingContextManager:
         return "you are in a with-block"
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print('LoggingContextManager.__exit__() [type:{}, value:{}, traceback:{}]'
+        if exc_type is None:
+            print('LoggingContextManager.__exit__()')
+        else:
+            print('LoggingContextManager.__exit__() [type:{}, value:{}, traceback:{}]'
             .format(exc_type, exc_val, exc_tb))
+        # exceptions are propagated if the return value is false
+        # since None = False, no return value will propagate exceptions
+        # to swallow the exception, simply return True
         return

2d1e332ab58c9e06599ba8fcbda8357b9bd22732
the-james-burton <the.james.burton@gmail.com>
2018-02-02 06:57:49 +0000

    introspection examples

---
+++ beyond-basics/13-introspection/introspection_test.py
@@ -0,0 +1,24 @@
+import unittest
+
+class TestHandler(unittest.TestCase):
+
+    def testIntrospection(self):
+        i = 7
+        # dir returns a list of attributes of an object...
+        print(dir(i))
+        # getattr returns the value of an attribute...
+        print(getattr(i, 'denominator'))
+        print(i.denominator)
+        # attributes can also be methods...
+        print(getattr(i, 'conjugate'))
+        # callable returns true if the attribute is a method...
+        print(callable(getattr(i, 'conjugate')))
+        # we can get other properties...
+        print(i.conjugate.__class__.__name__)
+        # hasattr returns true if the object has the given attribute...
+        print(hasattr(i, 'bit_length'))
+        # testing for attributes is slower than handling exceptions
+        # if the object does not have them
+
+
+

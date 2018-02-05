fe2fd639c13cdec0c2e85b323bf063f7454f8352
the-james-burton <the.james.burton@gmail.com>
2018-02-05 07:29:32 +0000

    inspect attributes

--- beyond-basics/13-introspection/introspector.py
+++ beyond-basics/13-introspection/introspector.py
@@ -1,3 +1,9 @@
+import inspect
+import reprlib
+
+import sorted_set
+
+
 def dump(obj):
     print("Type")
     print("====")
@@ -6,11 +12,20 @@ def dump(obj):
 
     print("Documentation")
     print("=============")
-    # TODO
+    print(inspect.getdoc(obj))
 
     print("Attributes")
     print("==========")
-    # TODO
+    attributes = sorted_set.SortedSet(dir(obj))
+    callables = sorted_set.SortedSet(
+        filter(lambda attribute: callable(obj, getattr(obj, attribute))),
+        attributes
+    )
+    assert callables < attributes
+    non_callables = attributes - callables
+    attributes = [(name, reprlib.repr(getattr(obj, name)))
+                             for name in attributes]
+    print_table(attributes, "Name", "Value")
 
     print("Methods")
     print("=======")

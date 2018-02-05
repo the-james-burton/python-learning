0e9c673f4b36534560206cc9151606d49960af8d
the-james-burton <the.james.burton@gmail.com>
2018-02-05 07:15:33 +0000

    initial inspector

---
+++ beyond-basics/13-introspection/introspector.py
@@ -0,0 +1,19 @@
+def dump(obj):
+    print("Type")
+    print("====")
+    print(type(obj))
+    print()
+
+    print("Documentation")
+    print("=============")
+    # TODO
+
+    print("Attributes")
+    print("==========")
+    # TODO
+
+    print("Methods")
+    print("=======")
+    # TODO
+
+
